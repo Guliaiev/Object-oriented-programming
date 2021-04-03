e52cbed8d75a47c87d38807eaf1f69235673debd
Guliaiev Andrey <andrguliaiev@gmail.com>
2021-04-03 19:03:33 +0300

    Post

---
+++ src/main/java/ru/netologi/manager/PostManager.java
@@ -0,0 +1,8 @@
+package ru.netologi.manager;
+import ru.netologi.domain.PostSource;
+public class PostManager {
+    private PostSource[]postSources;
+    public PostSource[] getPostSources(){
+        return null;
+    }
+}
